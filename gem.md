55e53abb3f21198960327bfb533420e644d58a81
Youcef Guichi <youcefelachir@gmail.com>
2025-04-04 09:09:09 +0200

    exploring rsa ..

---
+++ gem.md
@@ -0,0 +1,1 @@
+https://gist.github.com/denji/12b3a568f092ab951456
--- main.go
+++ main.go
@@ -1,49 +1,23 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
-	"os"
+    // "fmt"
+    // "io"
+    "net/http"
+    "log"
 )
 
-func root(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<h1>Hello, World!</h1>")
-	fmt.Println("Received request:", r.Method, r.URL.Path)
-	fmt.Println("Received request: TLS", r.TLS)
-	fmt.Println("Received request: header", r.Header)
-	fmt.Println("Received request: cookies", r.Cookies())
-	fmt.Println("Received request: content length", r.ContentLength)
-	fmt.Println("Received request: cookies", r.Form)
-	fmt.Println("Received request: body", r.GetBody)
-	fmt.Println("Received request: host", r.Host)
-	fmt.Println("Received request: method", r.Method)
-	fmt.Println("Received request: multupart", r.MultipartForm)
-	fmt.Println("Received request: pattern", r.Pattern)
-	fmt.Println("Received request: post form", r.PostForm)
-	fmt.Println("Received request: proto", r.Proto)
-	fmt.Println("Received request: proto Mahor", r.ProtoMajor)
-	fmt.Println("Received request: MulriForm", r.MultipartForm)
-	fmt.Println("Received request: remote address", r.RemoteAddr)
-	fmt.Println("Received request: request URI", r.RequestURI)
-	fmt.Println("Received request: response", r.Response)
-	fmt.Println("Received request: trailer", r.Trailer)
-	fmt.Println("Received request: encoding", r.TransferEncoding)
-	
-	
+func HelloServer(w http.ResponseWriter, req *http.Request) {
+    w.Header().Set("Content-Type", "text/plain")
+    w.Write([]byte("This is an example server.\n"))
+    // fmt.Fprintf(w, "This is an example server.\n")
+    // io.WriteString(w, "This is an example server.\n")
 }
 
 func main() {
-
-	http.HandleFunc("/", root)
-
-	port := ":8080"
-	err := http.ListenAndServe(port, nil)
-
-	if err != nil {
-		fmt.Printf("Error starting server: %v\n", err)
-		os.Exit(1)
-	}
-
-	fmt.Printf("Server started on localhost: %s\n", port)
-
+    http.HandleFunc("/hello", HelloServer)
+    err := http.ListenAndServeTLS(":5555", "server.crt", "server.key", nil)
+    if err != nil {
+        log.Fatal("ListenAndServe: ", err)
+    }
 }
